21373e2657dfd3527f51b212ad1feabc3365833f
Ross Jennings <rossjjennings@gmail.com>
2025-10-30 15:50:46 -0400

    Faster profile generation with Numba

--- jitter_correction/profile_data.py
+++ jitter_correction/profile_data.py
@@ -2,13 +2,130 @@ import numpy as np
 from numpy.random import random, randn, poisson
 import matplotlib.pyplot as plt
 from scipy import stats
+import numba as nb
 from dataclasses import dataclass
 from abc import ABCMeta, abstractmethod
 from typing import Any
+from collections.abc import Callable
 
 from .pulse_spec import PulseSpec
 from .mixins import NpzSerializable, Hdf5Serializable
 from .signal import fft_roll
+
+@nb.njit
+def _gamma_ampl(mean, modindex):
+    '''
+    A random amplitude drawn from a lognormal distribution.
+
+    Inputs
+    ------
+    mean:     The mean value
+    modindex: The modulation index of the distribution
+    '''
+    if modindex == 0:
+        result = mean
+    else:
+        shape = 1/modindex**2
+        scale = mean*modindex**2
+        result = np.random.gamma(shape, scale)
+    return result
+
+@nb.njit
+def _lognorm_ampl(mean, modindex):
+    '''
+    A random amplitude drawn from a lognormal distribution.
+
+    Inputs
+    ------
+    mean:     The mean value
+    modindex: The modulation index of the distribution
+    '''
+    sigma = np.sqrt(np.log(1 + modindex**2))
+    mu = np.log(mean) - 1/2*sigma**2
+    result = np.random.lognormal(mu, sigma)
+    return result
+
+@nb.njit(parallel=True)
+def _gen_pulses(
+    spec_data: np.recarray,
+    phase: np.ndarray,
+    n_pulses: int | np.integer,
+    snr: float | np.floating = np.inf,
+    ampl_dist: Callable = _gamma_ampl,
+) -> np.ndarray:
+    '''
+    Numba-compiled function to generate single pulses from a model with several
+    Gaussian components. `gen_pulses()` is a nice object-oriented wrapper
+    around this function.
+
+    Inputs
+    ------
+    spec_data:  Record array containing pulse specification
+                (see `PulseSpec` class).
+    phase:      Phase values (between -0.5 and 0.5).
+    n_pulses:   Number of pulses to generate.
+    snr:        Signal-to-noise ratio. If this is `np.inf`, no noise is added.
+    ampl_dist:  Distribution of amplitudes to use. Should be a Numba-compiled
+                function accepting `mean` and `modindex` parameters, and
+                returning a sample value.
+
+    Output
+    ------
+    pulses:     Pulses, as rows of a 2D array.
+    '''
+    pulses = np.zeros((n_pulses, phase.shape[0]))
+    for i in range(n_pulses):
+        for c in spec_data:
+            amplitude = ampl_dist(c.amplitude, c.modindex)
+            loc = c.loc + c.fj*c.width*np.random.randn()
+            chi = (phase - loc)/c.width
+            pulses[i] += amplitude * np.exp(-chi**2/2)
+    for k in range(phase.shape[0]):
+        pulses[i,k] += np.random.randn()/snr
+    return pulses
+
+@nb.njit(parallel=True)
+def _gen_profiles(
+    spec_data: np.recarray,
+    phase: np.ndarray,
+    n_profiles: int | np.integer,
+    npprof: int | np.integer,
+    snr: float | np.floating = np.inf,
+    ampl_dist: Callable = _gamma_ampl,
+) -> np.ndarray:
+    '''
+    Numba-compiled function to generate profiles from a model with several
+    Gaussian components. `gen_profiles()` is a nice object-oriented wrapper
+    around this function.
+
+    Inputs
+    ------
+    spec_data:  Record array containing pulse specification
+                (see `PulseSpec` class).
+    phase:      Phase values (between -0.5 and 0.5).
+    n_profiles: Number of profiles to generate.
+    npprof:     Number of pulses to average for each profile.
+    snr:        Signal-to-noise ratio. If this is `np.inf`, no noise is added.
+    ampl_dist:  Distribution of amplitudes to use. Should be a Numba-compiled
+                function accepting `mean` and `modindex` parameters, and
+                returning a sample value.
+
+    Output
+    ------
+    profiles:   Profiles, as rows of a 2D array.
+    '''
+    profiles = np.zeros((n_profiles, phase.shape[0]))
+    for i in nb.prange(n_profiles):
+        for j in range(npprof):
+            for c in spec_data:
+                amplitude = ampl_dist(c.amplitude, c.modindex)
+                loc = c.loc + c.fj*c.width*np.random.randn()
+                chi = (phase - loc)/c.width
+                profiles[i] += amplitude * np.exp(-chi**2/2)
+        profiles[i] /= npprof
+        for k in range(phase.shape[0]):
+            profiles[i,k] += np.random.randn()/snr
+    return profiles
 
 @dataclass(slots=True)
 class ProfileData(NpzSerializable, Hdf5Serializable):
@@ -52,11 +169,11 @@ class ProfileData(NpzSerializable, Hdf5Serializable):
         yield self.profiles
 
 def gen_pulses(
+    spec: PulseSpec,
     phase: np.ndarray,
     n_pulses: int | np.integer = 5000,
     snr: float | np.floating = np.inf,
     ampl_dist: str = 'gamma',
-    spec: PulseSpec = PulseSpec(),
 ) -> ProfileData:
     '''
     Generate synthetic pulses from a model with several Gaussian components.
@@ -73,43 +190,32 @@ def gen_pulses(
     ------
     pulses : Pulses, as rows of a 2D array.
     '''
-    n_phase = len(phase)
-    profiles = np.zeros((n_pulses, n_phase))
+    ampl_dist_callables = {
+        'gamma': _gamma_ampl,
+        'lognorm': _lognorm_ampl,
+    }
+    try:
+        ampl_dist = ampl_dist_callables[ampl_dist]
+    except KeyError:
+        raise ValueError(f"Amplitude distribution '{ampl_dist}' not recognized")
 
-    for c in spec.components():
-        try:
-            a = 1/c.modindex**2
-        except ZeroDivisionError:
-            amplitudes = np.full(n_pulses, c.amplitude)
-        else:
-            if ampl_dist == 'gamma':
-                scale = c.amplitude*c.modindex**2
-                amplitudes = stats.gamma.rvs(size = n_pulses, a = a, scale = scale)
-            elif ampl_dist == 'lognorm':
-                s = np.sqrt(np.log(1 + c.modindex**2))
-                scale = c.amplitude/np.sqrt(1 + c.modindex**2)
-                amplitudes = stats.lognorm.rvs(size = n_pulses, s = s, scale = scale)
-            else:
-                raise ValueError(f"Amplitude distribution '{ampl_dist}' not recognized")
-        jitter_rms = c.fj*c.width
-        locs = c.loc + jitter_rms*randn(n_pulses)
+    pulses = _gen_pulses(
+        spec_data = spec.data,
+        phase = phase,
+        n_pulses = n_pulses,
+        snr = snr,
+        ampl_dist = ampl_dist,
+    )
 
-        args = (phase - locs[:,np.newaxis])**2/(2*c.width**2)
-        profiles += amplitudes[:,np.newaxis] * np.exp(-args)
-
-    if np.any(snr != np.inf):
-        if np.ndim(snr) != 0:
-            snr = snr[..., np.newaxis]
-        profiles += randn(n_pulses, n_phase)/snr
-    return ProfileData(phase, profiles)
+    return ProfileData(phase, pulses)
 
 def gen_profiles(
+    spec: PulseSpec,
     phase: np.ndarray,
     n_profiles: int | np.integer = 10,
     npprof: int | np.integer = 1000,
     snr: float | np.floating = np.inf,
     ampl_dist: str = 'gamma',
-    spec: PulseSpec = PulseSpec(),
 ) -> ProfileData:
     '''
     Generate average profiles from a model with several Gaussian components.
@@ -128,47 +234,32 @@ def gen_profiles(
     ------
     profiles : Profiles, as rows of a 2D array.
     '''
-    n_phase = len(phase)
-    profiles = np.empty((n_profiles, n_phase))
-    pulses = np.empty((npprof, n_phase))
+    ampl_dist_callables = {
+        'gamma': _gamma_ampl,
+        'lognorm': _lognorm_ampl,
+    }
+    try:
+        ampl_dist = ampl_dist_callables[ampl_dist]
+    except KeyError:
+        raise ValueError(f"Amplitude distribution '{ampl_dist}' not recognized")
 
-    for i in range(n_profiles):
-        pulses.fill(0)
-        for c in spec.components():
-            try:
-                a = 1/c.modindex**2
-            except ZeroDivisionError:
-                amplitudes = np.full(npprof, c.amplitude)
-            else:
-                if ampl_dist == 'gamma':
-                    scale = c.amplitude*c.modindex**2
-                    amplitudes = stats.gamma.rvs(size = npprof, a = a, scale = scale)
-                elif ampl_dist == 'lognorm':
-                    s = np.sqrt(np.log(1 + c.modindex**2))
-                    scale = c.amplitude/np.sqrt(1 + c.modindex**2)
-                    amplitudes = stats.lognorm.rvs(size = npprof, s = s, scale = scale)
-                else:
-                    raise ValueError(f"Amplitude distribution '{ampl_dist}' not recognized")
-            jitter_rms = c.fj*c.width
-            locs = c.loc + jitter_rms*randn(npprof)
-
-            args = (phase - locs[:,np.newaxis])**2/(2*c.width**2)
-            pulses += amplitudes[:,np.newaxis] * np.exp(-args)
-        profiles[i,:] = np.mean(pulses, axis=0)
-
-    if np.any(snr != np.inf):
-        if np.ndim(snr) != 0:
-            snr = snr[..., np.newaxis]
-        profiles += randn(n_profiles, n_phase)/snr
+    profiles = _gen_profiles(
+        spec.data,
+        phase,
+        n_profiles,
+        npprof,
+        snr,
+        ampl_dist,
+    )
 
     return ProfileData(phase, profiles)
 
 def gen_pseudo_profiles(
+    spec: PulseSpec,
     phase: np.ndarray,
     n_profiles: int | np.integer = 100,
     npprof: int | np.integer = 10000,
     snr: float | np.floating = np.inf,
-    spec: PulseSpec = PulseSpec(),
 ) -> ProfileData:
     '''
     Generate synthetic "average profiles" from a model with several Gaussian
@@ -204,11 +295,11 @@ def gen_pseudo_profiles(
     return data
 
 def shift_template(
-        phase: np.ndarray,
-        shifts: int | np.integer,
-        snr: float | np.floating = np.inf,
-        spec: PulseSpec = PulseSpec(),
-    ) -> ProfileData:
+    spec: PulseSpec,
+    phase: np.ndarray,
+    shifts: int | np.integer,
+    snr: float | np.floating = np.inf,
+) -> ProfileData:
     '''
     Generate synthetic profiles by shifting a template.
 
