0354fe3d15b893fd762e6025f10ac75c5e2b8b37
Ross Jennings <rossjjennings@gmail.com>
2026-02-20 09:57:11 -0500

    Start SkewnessRegressionEstimator

--- jitter_correction/skewness.py
+++ jitter_correction/skewness.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 from dataclasses import dataclass
 
 from .profile_data import ProfileData
-from .toas import TemplateMatchingEstimator
+from .toas import TemplateMatchingEstimator, ToaResult
 from .mixins import RecordContainer
 
 eps=np.finfo(np.float64).eps
@@ -36,7 +36,7 @@ def skewness_coeff(
     '''
     inds, = np.where(lags == 0)
     zero_lag_bin = inds[0]
-    sl = slice(zero_lag_bin-nlags, zero_lag_bin+nlags+1)
+    sl = slice(zero_lag_bin - nlags, zero_lag_bin + nlags + 1)
     coeffs = np.polyfit(lags[sl], skewness[sl], 5)
     return coeffs[2]
 
@@ -45,34 +45,71 @@ def calc_skewness_coeffs(data: ProfileData) -> np.ndarray:
     Calculate skewness coefficients for a set of profiles.
     '''
     n_profiles, n_bins = data.profiles.shape
-    lags = np.empty(2*n_bins-1)
+    lags = np.empty(2*n_bins - 1)
     lags[n_bins-1:] = np.linspace(0, 1, n_bins)
     lags[:n_bins-1] = -np.linspace(0, 1, n_bins)[:0:-1]
 
-    skewness_fns = np.empty((n_profiles, 2*n_bins-1))
+    skewness_fns = np.empty((n_profiles, 2*n_bins - 1))
     for i, profile in enumerate(data.profiles):
         skewness_fn = skewness_function(profile)
         skewness_fns[i] = skewness_fn
     skewness_coeffs = np.array([
-        skewness_coeff(lags, skewness_fn, nlags=129) for skewness_fn in skewness_fns
+        skewness_coeff(lags, skewness_fn, nlags=129)
+        for skewness_fn in skewness_fns
     ])
 
     return skewness_coeffs
 
-class ToaSkewnessResult(NamedTuple):
-    toa: float | np.floating
-    ampl: float | np.floating
-    skewness_coeff: float | np.floating
+@dataclass(slots=True, repr=False)
+class ToaSkewnessResult(ToaResult):
+    toa: np.floating
+    ampl: np.floating
+    offset: np.floating
+    noise_level: np.floating
+    toa_error: np.floating
+    ampl_error: np.floating
+    offset_error: np.floating
+    skewness_coeff: np.floating
 
 @dataclass(slots=True)
 class ToaSkewnessResults(RecordContainer[ToaSkewnessResult]):
     pass
 
+class SkewnessRegressionEstimator:
+    '''
+    A TOA estimator based on the skewness coefficient
+    '''
+    def __init__(self, template: np.ndarray, predictor_coeffs: np.ndarray):
+        self.tm_estimator = TemplateMatchingEstimator(template)
+        self.predictor_coeffs = predictor_coeffs
+
+    def estimate_toa(
+        self,
+        profile: np.ndarray,
+        tol: float | np.floating = np.sqrt(np.finfo(np.float64).eps),
+        noise_level: float | np.floating | None = None,
+    ) -> ToaSkewnessResult:
+        '''
+        Estimate a TOA using the skewness model.
+
+        Parameters
+        ----------
+        template: The profile model to use for fitting
+        predictor_coeffs: Coefficients of the skewness to use in correction
+        data: Profiles for which to calculate TOAs, as a `ProfileData` object
+        tol: Relative tolerance for optimization (in bins).
+
+        Returns
+        -------
+        result: `ToaSkewnessResult` object containing parameter values and
+            their uncertainties.
+        '''
+        pass
+
 def get_toas_skewness(
     template: np.ndarray,
     predictor_coeffs: np.ndarray,
     data: ProfileData,
-    dt: float | np.floating = 1.,
     tol: float | np.floating = np.sqrt(eps),
 ) -> ToaSkewnessResults:
     '''
@@ -83,7 +120,6 @@ def get_toas_skewness(
     `template`: The profile model to use for fitting
     `predictor_coeffs`: Coefficients of the skewness to use in correction
     `data`:   Profiles for which to calculate TOAs, as a `ProfileData` object
-    `dt`:     The width of each phase bin in the profile. Sets the units of the TOA.
     `tol`:    Relative tolerance for optimization (in bins).
     '''
     estimator = TemplateMatchingEstimator(template)
@@ -92,8 +128,7 @@ def get_toas_skewness(
     toa_corrections = np.polyval(predictor_coeffs, skewness_coeffs)
     toas_skewness = initial_results.toa - toa_corrections
 
-    records = np.rec.fromarrays( # type: ignore
-        [
+    records = np.rec.fromarrays([ # type: ignore
             toas_skewness,
             initial_results.ampl,
             skewness_coeffs,
